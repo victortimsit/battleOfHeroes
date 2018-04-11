049983f3d3c24b51b7525bcd7db9429bc605e615
vtimsit <victor.timsit@hetic.net>
2018-04-12 00:02:19 +0200

    :lipstick: Sign in

--- app/index.php
+++ app/index.php
@@ -9,9 +9,9 @@
   // $signInErrors;
   $rooter = new Rooter($inputs, $pdo);
 
-  echo '<pre>';
-  print_r($signUpErrors);
-  echo '</pre>';
+  // echo '<pre>';
+  // print_r($signUpErrors);
+  // echo '</pre>';
 
   function connection($account, $id) {
     $_SESSION['user'] = $account;
@@ -43,7 +43,7 @@
         <h1>Title</h1>
         <a href="./?action=deconnection">Sign out</a>
         <!--build:css styles/styles.min.css-->
-        <link rel="stylesheet" href="styles/css/base/styles.css" />
+        <link rel="stylesheet" href="styles/css/main.css" />
         <!--endbuild-->
       </head>
       <body>
@@ -60,24 +60,46 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Register</title>
-      <link rel="stylesheet" href="styles/css/base/register.css" />
+      <link rel="stylesheet" href="styles/css/main.css" />
     </head>
     <body>
-      <div class="register">
-          <form action="#" method="post">
-              <label class="register__label" for="user_name">User name</label>
-              <input class="register__input" type="text" name="user_name" placeholder="User name">
-              <label class="register__label" for="password">Password</label>
-              <input class="register__input" type="password" name="password" placeholder="Password">
-              <div class="register__button">
-                <label>
-                <input class="register__submit" type="submit">
-                <div>Connexion</div>
-                </label>
-              </div>
+      <div class="signUp">
+        <h1 class="signUp__title">Log in</h1>
+          <form class="signUp__form" action="#" method="post">
+              <label class="signUp__label" for="user_name">User name</label>
+              <input class="signUp__input" type="text" name="user_name" placeholder="User name">
+              <p class="signUp__error signUp__error<?= !empty($signInErrors['user_name']) ? $signInErrors['user_name'] : false ?>">
+              <?php if(!empty($signInErrors['user_name'])) { ?>
+              <?= $signInErrors['user_name'] == '--empty' ? 'User name is required' : false?>
+              <?php } ?>
+              </p>
+              <label class="signUp__label" for="password">Password</label>
+              <input class="signUp__input" type="password" name="password" placeholder="Password">
+              <p class="signUp__error signUp__error<?= !empty($signInErrors['password']) ? $signInErrors['password'] : false ?>">
+              <?php if(!empty($signInErrors['password'])) { ?>
+              <?= $signInErrors['password'] == '--empty' ? 'Password is required' : false?>
+              <?php } ?>
+              <?php if(!empty($signInErrors['connection'])) { ?>
+              <?= $signInErrors['connection'] == '--invalid' ? 'Wrong user name or password' : false?>
+              <?php } ?>
+              </p>
+              <label>
+              <input class="signUp__radio--hidden" type="submit">
+              <div class="signUp__connexion">Connexion</div>
+              </label>
           </form>
-          <a href="signUp.php">Create a account</a>
-      </div>
+          <div class="signIn__lines">
+            <div class="signIn__line"></div>
+            <p class="signIn__or">or</p>
+            <div class="signIn__line"></div>
+          </div>
+
+          <a class="signIn__signUp" href="signUp.php">
+          <input class="signUp__radio--hidden" type="submit">
+          <div class="signUp__button">Create a account</div>
+          </a>         
+
+        </div>
     </body>
     </html>
     <?php
